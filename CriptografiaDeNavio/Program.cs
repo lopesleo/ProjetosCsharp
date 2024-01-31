d74a7adeb1455adf772878069cfc08d1cee694f9
Leonardo Lopes <leonardo.lopes@golivetech.com.br>
2024-01-31 01:01:46 -0300

    feat: add criptografiadenavio and changes in menordistaciaentrearrays

--- CriptografiaDeNavio/Program.cs
+++ CriptografiaDeNavio/Program.cs
@@ -1,2 +1,85 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace CriptografiaDeNavio
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mensagemCriptografada = "10010110 11110111 01010110 00000001 00010111 00100110 01010111 00000001 00010111 01110110 01010111 00110110 11110111 11010111 01010111 00000011";
+            string[] Blocos = mensagemCriptografada.Split(' ');
+            Console.WriteLine("Mensagem original: " + mensagemCriptografada);
+            Blocos = InverteBitsFinais(Blocos);
+            Console.WriteLine("Mensagem com bits finais invertidos: ");
+            foreach (string b in Blocos)
+            {
+                Console.Write(b.ToString() + " ");
+            }
+            Blocos = Inverte4Bits(Blocos);
+            Console.WriteLine("\nMensagem com 4 bits invertidos: ");
+            foreach (string b in Blocos)
+            {
+                Console.Write(b.ToString() + " ");
+            }
+
+
+
+            string mensagemFinal = ConverteBinarioParaAscii(Blocos);
+            Console.WriteLine("\nMensagem desencriptada: " + mensagemFinal);
+            Console.ReadLine();
+        }
+
+        public static string ConverteBinarioParaAscii(string[] mensagem)
+        {
+            string mensagemFinal = string.Empty;
+            foreach (string i in mensagem)
+            {
+                int ascii = Convert.ToInt32(i, 2);
+                char caractere = Convert.ToChar(ascii);
+                mensagemFinal += caractere;
+            }
+            return mensagemFinal;
+        }
+
+        public static string[] InverteBitsFinais(string[] mensagem)
+        {
+            for (int i = 0; i < mensagem.Length; i++)
+            {
+                char[] chars = mensagem[i].ToCharArray();
+
+                if (chars[chars.Length - 1] == '0')
+                {
+                    chars[chars.Length - 1] = '1';
+                }
+                else
+                {
+                    chars[chars.Length - 1] = '0';
+                }
+                if (chars[chars.Length - 2] == '0')
+                {
+                    chars[chars.Length - 2] = '1';
+                }
+                else
+                {
+                    chars[chars.Length - 2] = '0';
+                }
+                mensagem[i] = new string(chars);
+            }
+
+            return mensagem;
+        }
+
+        public static string[] Inverte4Bits(string[] mensagem)
+        {
+            string[] mensagemFinal = new string[mensagem.Length];
+            for (int i = 0; i < mensagem.Length; i++)
+            {
+                var parte1 = mensagem[i].Substring(0, 4);
+                var parte2 = mensagem[i].Substring(4);
+                mensagemFinal[i] = parte2 + parte1;
+            }
+            return mensagemFinal;
+        }
+    }
+
+}
--- MenorDistanciaEntreArrays/Program.cs
+++ MenorDistanciaEntreArrays/Program.cs
@@ -1,58 +1,111 @@
-﻿using System;
+﻿
+using System;
 
-class Program
+namespace MenorDistanciaEntreArrays
 {
-    static void Main(string[] args)
+    class Program
     {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("digite 1 para usar arrays aleatorios ou 2 para inserir arrays ");
+            int escolha = int.Parse(Console.ReadLine());
+            List<int> array1 = new List<int>();
+            List<int> array2 = new List<int>();
 
-        int[] array1 = new int[10];
-        int[] array2 = new int[10];
-        PopulaArray(array1);
-        PopulaArray(array2);
-        
-        Console.WriteLine(EscreveArray(array1));
-        Console.WriteLine(EscreveArray(array2));
-        MenorDistancia(array1, array2);
-
-       
-    }
-    public static string EscreveArray(int[] array)
-    {
-        string arrayEscrito = "";
-        foreach (int i in array)
-        {
-            arrayEscrito += i + " ";
-        }
-        return arrayEscrito;
-    }
-    public static int PopulaArray(int[] array)
-    {
-        Random random = new();
-        for (int i = 0; i < array.Length; i++)
-        {
-            array[i] = random.Next(-100, 100);
-        }
-        return array.Length;
-    }
-    public static int MenorDistancia(int[] array1, int[] array2)
-    {
-        int menorDistancia = int.MaxValue;
-        int[] numerosUtilizados = new int[2];
-        foreach (int i in array1)
-        {
-            foreach (int j in array2)
+            if (escolha == 1)
             {
-                int distancia = Math.Abs(i - j);
-                if (distancia < menorDistancia)
+                PopulaArray(array1);
+                PopulaArray(array2);
+            }
+            else if (escolha == 2)
+            {
+                Console.WriteLine("Digite os números do primeiro array (digite 'pronto' para finalizar):");
+                while (true)
                 {
-                    menorDistancia = distancia;
-                    numerosUtilizados[0] = i;
-                    numerosUtilizados[1] = j;
+                    string linha = Console.ReadLine();
+                    if (linha.ToLower() == "pronto")
+                    {
+                        break;
+                    }
+                    int numero;
+                    if (int.TryParse(linha, out numero))
+                    {
+                        array1.Add(numero);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Entrada inválida. Digite um número ou 'pronto' para finalizar.");
+                    }
+                }
+
+                Console.WriteLine("Digite os números do segundo array (digite 'pronto' para finalizar):");
+                while (true)
+                {
+                    string linha = Console.ReadLine();
+                    if (linha.ToLower() == "pronto")
+                    {
+                        break;
+                    }
+                    int numero;
+                    if (int.TryParse(linha, out numero))
+                    {
+                        array2.Add(numero);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Entrada inválida. Digite um número ou 'pronto' para finalizar.");
+                    }
                 }
             }
+            else
+            {
+                Console.WriteLine("Escolha inválida");
+            }
+
+            Console.WriteLine(EscreveArray(array1));
+            Console.WriteLine(EscreveArray(array2));
+            MenorDistancia(array1, array2);
+            Console.ReadLine();
         }
-        Console.WriteLine("Menor distancia: " + menorDistancia);
-        Console.WriteLine("Numeros utilizados: " + numerosUtilizados[0] + " e " + numerosUtilizados[1]);
-        return menorDistancia;
+
+        public static string EscreveArray(List<int> array)
+        {
+            string arrayEscrito = "";
+            foreach (int i in array)
+            {
+                arrayEscrito += i + " ";
+            }
+            return arrayEscrito;
+        }
+
+        public static void PopulaArray(List<int> array)
+        {
+            Random random = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                array.Add(random.Next(-100, 100));
+            }
+        }
+
+        public static void MenorDistancia(List<int> array1, List<int> array2)
+        {
+            int menorDistancia = int.MaxValue;
+            int[] numerosUtilizados = new int[2];
+            foreach (int i in array1)
+            {
+                foreach (int j in array2)
+                {
+                    int distancia = Math.Abs(i - j);
+                    if (distancia < menorDistancia)
+                    {
+                        menorDistancia = distancia;
+                        numerosUtilizados[0] = i;
+                        numerosUtilizados[1] = j;
+                    }
+                }
+            }
+            Console.WriteLine("Menor distancia: " + menorDistancia);
+            Console.WriteLine("Numeros utilizados: " + numerosUtilizados[0] + " e " + numerosUtilizados[1]);
+        }
     }
 }
